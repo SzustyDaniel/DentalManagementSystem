3e1cad033548556f413cb352e2b0144f9e3dc649
Daniel Szuster <danielszu23@gmail.com>
2019-06-17 17:58:36 +0300

    Added Control For users Added Some logic for user models

--- DMS_Solution/StaffClientApplication/ViewModels/AppMainWindowViewModel.cs
+++ DMS_Solution/StaffClientApplication/ViewModels/AppMainWindowViewModel.cs
@@ -1,16 +1,30 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using StaffClientApplication.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Controls;
 
 namespace StaffClientApplication.ViewModels
 {
     public class AppMainWindowViewModel : BindableBase
     {
+        private UserControl _currentView;
+        public UserControl CurrentView
+        {
+            get { return _currentView; }
+            set { SetProperty(ref _currentView, value); }
+        }
+
         public AppMainWindowViewModel()
         {
+            CurrentView = new LoginUC();
+        }
 
+        private void SwitchView(UserControl uc)
+        {
+            CurrentView = uc;
         }
     }
 }
