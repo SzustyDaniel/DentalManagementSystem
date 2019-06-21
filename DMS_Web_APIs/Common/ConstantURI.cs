0e06fe1bfada9734243c0fc43647e1d693a4a9de
Daniel Szuster <danielszu23@gmail.com>
2019-06-21 09:25:32 +0300

    Preparing for implementation

--- DMS_Web_APIs/Common/ConstantURI.cs
+++ DMS_Web_APIs/Common/ConstantURI.cs
@@ -8,5 +8,6 @@ namespace Common
     {
         public const string queueServerURI = "https://localhost:44305/";
         public const string usersServerURI = "http://localhost:53512/";
+        public const string managementServerURI = "http://localhost:56358/";
     }
 }
---
+++ DMS_Web_APIs/ManagementService/Controllers/ManagementController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManagementService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ManagementController : ControllerBase
+    {
+
+    
+    }
+}
