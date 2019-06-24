4f7f43cb91ab3a06851a3af99b23c6291c321d83
Daniel Szuster <danielszu23@gmail.com>
2019-06-24 16:12:19 +0300

    Added MORE TESTS!!!!!!

---
+++ DMS_Solution/StaffStationClient.Tests/Mocks/TreatmentMock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StaffStationClient.Tests.Mocks
+{
+    public class TreatmentMock
+    {
+        public DateTime TreatmentDate { get; set; }
+
+        public int EmployeeId { get; set; }
+        public EmployeeMock Employee { get; set; }
+
+        public int CustomerId { get; set; }
+        public CustomerMock Customer { get; set; }
+    }
+}
--- DMS_Solution/StaffStationClient.Tests/StaffControlUCViewModelTests.cs
+++ DMS_Solution/StaffStationClient.Tests/StaffControlUCViewModelTests.cs
@@ -3,11 +3,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Prism.Events;
 using StaffStationClient.Services;
 using StaffStationClient.Tests.Mocks;
+using StaffStationClient.ViewModels;
 
 namespace StaffStationClient.Tests
 {
     [TestClass]
-    public class LoginUCViewModelTests
+    public class StaffControlUCViewModelTests
     {
         private IEventAggregator eventAggregator;
         private IHttpActions httpActions;
@@ -22,9 +23,16 @@ namespace StaffStationClient.Tests
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void ExecuteCallNextCommandAsync_CallToNextClient_NextClientWasCalled()
         {
+            // Arrange
+            StaffControlUCViewModel viewModel = new StaffControlUCViewModel(eventAggregator, httpActions, dialogService);
+            viewModel.Model = new Models.StationModel() { StationNumber = 1, StationServiceType = Common.ServiceType.Nurse };
 
+            // Act
+
+
+            // Assert
         }
     }
 }
