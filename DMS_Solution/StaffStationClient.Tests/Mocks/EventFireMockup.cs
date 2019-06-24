566e380cca4d9b31c9b4f2a8fa4100ae793200f0
Daniel Szuster <danielszu23@gmail.com>
2019-06-24 17:42:51 +0300

    Added Tests for Staff client for now finished with it.

--- DMS_Solution/StaffStationClient.Tests/Mocks/EventFireMockup.cs
+++ DMS_Solution/StaffStationClient.Tests/Mocks/EventFireMockup.cs
@@ -1,5 +1,6 @@
 ﻿using Prism.Events;
 using StaffStationClient.Models;
+using StaffStationClient.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,30 @@ namespace StaffStationClient.Tests.Mocks
 {
     public class EventFireMockup
     {
+        private IEventAggregator eventAggregator;
 
+        public StationModel StationModel { get; set; }
+
+        public EventFireMockup(IEventAggregator eventAggregator)
+        {
+            this.eventAggregator = eventAggregator;
+            StationModel = new StationModel()
+            {
+                StationNumber = 1,
+                StationServiceType = Common.ServiceType.Nurse,
+                EmployeeFirstName = "Daniel",
+                EmployeeLastName = "Szuster",
+                EmployeeId = 1,
+                Password = "1234",
+                UserName = "daniel_s"
+            };
+
+        }
+
+        public void SendModel()
+        {
+            eventAggregator.GetEvent<SendModelEvent>().Publish(StationModel);
+        }
 
     }
 }
