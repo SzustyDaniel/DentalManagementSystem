03805968039c9ef731457424e9c594706d05391f
Daniel Szuster <danielszu23@gmail.com>
2019-06-25 21:55:51 +0300

    Added Tests

--- DMS_Web_APIs/ManagemenrService.Test/UsersApiServiceMock.cs
+++ DMS_Web_APIs/ManagemenrService.Test/UsersApiServiceMock.cs
@@ -1,8 +1,21 @@
-﻿using ManagementService.Services;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Common.UserModels;
+using ManagementService.Services;
 
 namespace Tests
 {
     internal class UsersApiServiceMock : UsersApiService
     {
+        public override Task<List<DailyEmployeeReport>> GetUsersTtreatments(DateTime date)
+        {
+            throw new NotImplementedException();
+
+            List<DailyEmployeeReport> reports = new List<DailyEmployeeReport>()
+            {
+                new DailyEmployeeReport() { Date = DateTime.Parse("2019-06-24"), FirstName = "Daniel", LastName = "Szuster"}
+            };
+        }
     }
 }
